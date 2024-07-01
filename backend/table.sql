376b10e75200effecd6642441648d5ae077c0215
ReinaLalitta <lalittaruby@gmail.com>
2024-07-01 04:15:04 +0300

    Cafe Management Systems

--- backend/table.sql
+++ backend/table.sql
@@ -16,3 +16,26 @@ create table category(
     name varchar(255) NOT NULL,
     primary key(id)
 );
+
+create table product(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    categoryId integer NOT NULL,
+    description varchar(255),
+    price integer,
+    status varchar(20),
+     primary key(id)
+);
+
+create table bill(
+    id int NOT NULL AUTO_INCREMENT,
+    uuid varchar(200) NOT NULL,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    contactNumber varchar(20) NOT NULL,
+    paymentMethod varchar(50) NOT NULL,
+    total int NOT NULL,
+    productDetails JSON DEFAULT NULL,
+    createdBy varchar(255) NOT NULL,
+     primary key(id)
+);
